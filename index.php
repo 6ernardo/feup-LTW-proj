8e7154604920aec7551394073c0ac7ec6511ee4c
bernardo <6ernardocampos@gmail.com>
2023-06-24 16:10:34 +0100

    First templates

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types = 1);
+
+require_once('templates/common.tpl.php');
+
+drawHeader();
+drawFooter();
+
+?>
---
+++ templates/common.tpl.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types = 1); ?>
+
+<?php function drawHeader() { ?>
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Ticket Site</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <h1>Ticket Site</h1>
+                <?php
+                    drawLoggedOff();
+                ?>
+<?php } ?>
+
+<?php function drawLoggedIn() { ?>
+        <p>Edit Profile</p>
+        <p>Log Off</p>
+    </nav>
+</header>
+<main>
+<?php } ?>
+
+<?php function drawLoggedOff() { ?>
+        <p>Login<p>
+        <p>Register<p>
+    </nav>
+</header>
+<main>
+    <p>Welcome to our Support Ticket Website. Login or register a new account to get started!</p>
+<?php } ?>
+
+
+<?php function drawFooter() { ?>
+        </main>
+        <footer>
+            LTW Ticket Site 2023
+        </footer>
+    </body>
+</html>
+<?php } ?>
+
