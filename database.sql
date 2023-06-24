a8a13ae06a5c50e226ed1675d78bea2a93a8b335
bernardo <6ernardocampos@gmail.com>
2023-06-24 01:03:26 +0100

    First version of database

---
+++ database.sql
@@ -0,0 +1,79 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS ticket_inquiries;
+DROP TABLE IF EXISTS hashtags;
+DROP TABLE IF EXISTS status;
+DROP TABLE IF EXISTS faq;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    role_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE departments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE agent_departments (
+    agent_id INTEGER,
+    department_id INTEGER,
+    PRIMARY KEY (agent_id, department_id),
+    FOREIGN KEY (agent_id) REFERENCES users(id),
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE tickets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    subject TEXT NOT NULL,
+    content TEXT,
+    submitter_id INTEGER NOT NULL,
+    assignee_id INTEGER,
+    department_id INTEGER,
+    status_id INTEGER NOT NULL,
+    FOREIGN KEY (submitter_id) REFERENCES users(id),
+    FOREIGN KEY (assignee_id) REFERENCES users(id),
+    FOREIGN KEY (department_id) REFERENCES departments(id),
+    FOREIGN KEY (status_id) REFERENCES status(id)
+);
+
+CREATE TABLE ticket_inquiries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ticket_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    FOREIGN KEY (ticket_id) REFERENCES tickets(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+--Need to create table mapping hastags to tickets?
+CREATE TABLE hashtags (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE status (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+--Represents a single question of the FAQ
+CREATE TABLE faq (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    question TEXT NOT NULL,
+    answer TEXT NOT NULL
+);
+
+
+
